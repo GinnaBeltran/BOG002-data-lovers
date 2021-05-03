c4f5c33c0c3a1b52aee16aaaa1a9b28d8a7b223f
Ginna Alejandra Beltrán Lara <GinnaBeltran@iMac-de-Ginna.local>
2021-05-03 13:40:49 -0500

    funcion orden

--- src/data.js
+++ src/data.js
@@ -1,9 +1,112 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+/* const filtrado= {
+  filtrarName,
+} */
 
-export const anotherExample = () => {
+export const filtrarName = (name, pokemones) => {
+  return pokemones.filter(pokemon => pokemon.name.includes(name));
+}
+
+export const filtrarNum = (num, pokemones) => {
+  return pokemones.filter(pokemon => pokemon.num.includes(num));
+}
+
+export const filtrarType = (type, pokemones) => {
+  return pokemones.filter(pokemon => pokemon.type.includes(type));
+}
+
+export const ordenPok =(ordenSelecion, pokemon)=>{
+  if (ordenSelecion==0){
+    let personajesOrdenadosAz= pokemon.sort((a, b)=> {
+      if (a.name.toLowerCase() > b.name.toLowerCase()) return 1;
+      if (a.name.toLowerCase() < b.name.toLowerCase()) return -1;
+      return 0;
+    });
+    return personajesOrdenadosAz;
+  }
+
+  if (ordenSelecion==1) {
+    let personajesOrdenadosZa= pokemon.sort((a, b)=> {
+      if (a.name.toLowerCase() > b.name.toLowerCase()) return -1;
+      if (a.name.toLowerCase() < b.name.toLowerCase()) return 1;
+      return 0;
+    });
+    return personajesOrdenadosZa;
+  }
+}
+
+export const ordenNum =(pokemon)=>{
+  /* if (ordenSelecion==0){ */
+    let personajesOrdenadosAz= pokemon.sort((a, b)=> {
+      if (a.num > b.num) return 1;
+      if (a.num < b.num) return -1;
+      return 0;
+    });
+    return personajesOrdenadosAz;
+  }
+
+/*   if (ordenSelecion==1) {
+    let personajesOrdenadosZa= pokemon.sort((a, b)=> {
+      if (a.num > b.num) return -1;
+      if (a.num < b.num) return 1;
+      return 0;
+    });
+    return personajesOrdenadosZa;
+  } */
+
+/* export const ordenAz= (personajespok)=>{
+  let personajesOrdenados= personajespok.sort(function (a, b) {
+  if (a.name > b.name) {
+    return 1;
+  }
+  if (a.name < b.name) {
+    return -1;
+  }
+  // a must be equal to b
+  return 0;
+});
+console.log(personajesOrdenados);
+return personajesOrdenados;
+}
+/* console.log(ordenAz); */
+
+/* export const ordenZa= (personajespok)=>{
+  let personajesOrdenados= personajespok.name.sort(function (a, b) {
+  if (a.name < b.name) {
+    return 1;
+  }
+  if (a.name > b.name) {
+    return -1;
+  } */
+  // a must be equal to b
+  /* return 0;
+});
+console.log(personajesOrdenados);
+
+return personajesOrdenados;
+} */
+/* console.log(ordenZa); */
+/* console.log(personajespok[0].name); */
+
+/* export const OrdenarDescendente = function (objectPersonajes){   
+   let personajesOrdenadosDes = objectPersonajes.sort((a,b) =>{  
+       if(a.name.toLocaleLowerCase()<b.name.toLocaleLowerCase()) return 1;   
+        if(a.name.toLocaleLowerCase()>b.name.toLocaleLowerCase()) return -1;   
+        return 0;    })      
+        return personajesOrdenadosDes;
+   } */
+/* function ordenPok(firstEl, secondEl){
+  return secondEl["num"]- firstEl["num"];
+}
+
+export const ordenTargetas= (pokemones) =>{
+  console.log("ok");
+  return pokemones.num.sort(ordenPok);
+} */
+
+/* export const anotherExample = () => {
   return 'OMG';
-};
+}; */
+
+
--- src/main.js
+++ src/main.js
@@ -1,6 +1,105 @@
-import { example } from './data.js';
+import  { filtrarName,filtrarNum,filtrarType,ordenPok, ordenNum} from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+const personajespok = data.pokemon; 
+/* console.log(personajespok); */
+
+function mostrarTargeta (personajespok){
+   const TargetasPok = document.getElementById("targetas");
+   TargetasPok.innerHTML= '';
+
+for (let i= 0; i <personajespok.length; i++){
+     /* let namePok= personajespok[i].name;
+     let numPok= personajespok[i].num;
+     let aboutPok= personajespok[i].about;
+     let imgPok= personajespok[i].img; */
+
+     let unidadTargeta= document.createElement("div");
+     let imgpok= document.createElement("img");
+     let nombres= document.createElement("button");
+     let numeros= document.createElement("p");
+
+     unidadTargeta.className="unidadTargeta";
+     imgpok.className="imgTargetas";
+     nombres.id="nombresPok";
+     nombres.className="nombresPoke";
+     numeros.className="numeroPok";
+
+
+     imgpok.src= personajespok[i].img;
+     nombres.innerHTML=personajespok[i].name;
+     numeros.innerHTML=personajespok[i].num;
+
+     unidadTargeta.appendChild(numeros);
+     unidadTargeta.appendChild(imgpok);
+     unidadTargeta.appendChild(nombres);
+     TargetasPok.appendChild(unidadTargeta);
+}}
+
+mostrarTargeta(personajespok);
+
+// evento busqueda por nombre
+
+const barraBuscar= document.getElementById("busqueda");
+barraBuscar.addEventListener("keyup", buscarPok);
+function buscarPok(){
+    let ingreso= barraBuscar.value;
+    console.log(ingreso);
+    let resultadoBuscar= filtrarName(ingreso,personajespok);
+    /* mostrarTargeta(resultadoBuscar); */
+    let resultadoBuscarNum= filtrarNum(ingreso,personajespok);
+    /* mostrarTargeta(resultadoBuscar); */
+    let resultadoBuscarType= filtrarType(ingreso,personajespok);
+    /* mostrarTargeta(resultadoBuscar); */
+    let resultadoFinal= resultadoBuscar.concat(resultadoBuscarNum).concat(resultadoBuscarType);
+    mostrarTargeta(resultadoFinal);
+}
+
+// evento pokebola
+
+const btninicio = document.getElementById("pokebola");
+btninicio.addEventListener("click", function(){
+    mostrarhome();
+})
+
+/* btninicio.addEventListener("click", function(){
+    mostrarTargeta();
+}) */
+ 
+
+// evento mostrar contenido 2
+
+function mostrarhome (){
+    console.log(document.body.style.backgroundImage="url(../imagenes/home.jpg)")
+    inicio.style.display= "none";
+    contenido1.style.display="block";
+}
+
+//evento buscar
+
+const btnbuscar = document.getElementById("btnbuscar");
+btnbuscar.addEventListener("click", function(){
+    console.log("click en buscar");
+
+})
+
+const bntOrden = document.getElementById("orden");
+bntOrden.addEventListener("change", function(){
+let ordenSeleccionado = bntOrden.selectedIndex;
+let pokemonesOrdenados = ordenPok(ordenSeleccionado, personajespok);
+   mostrarTargeta(pokemonesOrdenados);
+})
+
+//evento bntver-todos
+
+const btntodos = document.getElementById("todos");
+btntodos.addEventListener("click", function(){
+
+    let ordenNumero= ordenNum(personajespok);
+    mostrarTargeta(ordenNumero);
+})
+
+
+console.log(filtrarName, filtrarNum, filtrarType, ordenPok, ordenNum, data);
